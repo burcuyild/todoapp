74f3f97239e8415978ce417f43f9f4e451a4110a
burcuyild <burcu.yild@hotmail.com>
2025-03-10 11:47:45 +0300

    lists

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {Todoitem} from "../todoitem";
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent implements OnInit {
+export class TodoComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
+private name = "Burcu";
+
+  // items = [
+  //   "item1", "item2", "item3", "item4"
+  // ]
+
+  items = [
+    { id: 1, description: "Breakfast", action: "Done"},
+    { id: 2, description: "Shoppping", action: "Done"},
+    { id: 3, description: "Tennis", action: "Todo"}
+  ]
+  getName(){
+    return this.name;
   }
+
+
 
 }
